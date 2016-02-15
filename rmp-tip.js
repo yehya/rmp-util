dfb27b8e98b6039a565b223536a0f64f010b14eb
Yehya Awad <yehyaawad@hotmail.com>
2016-02-14 22:23:56 -0500

    Change professorData to prfData bcz its 2 lng 2 type

--- rmp-tip.js
+++ rmp-tip.js
@@ -1,6 +1,6 @@
 
 /* hover pop up function that takes in the professor element for hovering over and professor data*/
-var rmptip = function(professorElement, professorData) {
+var rmptip = function(professorElement, prfData) {
 
   // contains the hotness images
   var HOTNESS_IMAGES = [
@@ -13,7 +13,7 @@ var rmptip = function(professorElement, professorData) {
   var professorTipPopUp = new Opentip(professorElement, {
   title: ('<span>' + '<img src=\'assets/smilebox.png\' height=\'42\' width=\'42\'>' +
         '<h1 class=\'profname\'>' +
-        professorData.profName +
+        prfData.profName +
         '</h1>' + '</span>'),
   // html for the title and content is not escaped for styling
   escapeTitle: false,
@@ -31,27 +31,20 @@ var rmptip = function(professorElement, professorData) {
 }
 );
 
-  professorTipPopUp.show(); // Shows the tooltip immediately
-  professorTipPopUp.hide(); // Hides the tooltip immediately
-  professorTipPopUp.prepareToShow(); // Shows the tooltip after the given delays. This could get interrupted
-  professorTipPopUp.prepareToHide();// prepares it to hide
-  professorTipPopUp.deactivate(); // hides it
-  professorTipPopUp.activate(); // shows it
-
   /*progress bar for the professor attributes*/
   var progressBar = '<span id=\'entireBar\'><span id=\'progressBar\'></span><a href=\'#\' id=\'A_3\'></a></span>';
 
   /* creates the project description headings, professor heading content and appends the progress bar */
   var profDescriptionHeading = function(profClass, profAttribute, profAttributeTitle, progressBar) {
-  return '<span class=\'' + profClass + '\'>' + '<h2 class=\'profheading\'>' + profAttributeTitle + '</h2>' +
-  '<h2 class=\'profheadingcontent\'>' + profAttribute + progressBar + '</h2>' + '</span>';
-};
+    return '<span class=\'' + profClass + '\'>' + '<h2 class=\'profheading\'>' + profAttributeTitle + '</h2>' +
+    '<h2 class=\'profheadingcontent\'>' + profAttribute + progressBar + '</h2>' + '</span>';
+  };
 
   var hotnessElement;
   /* Validates that the professor hotness that is passed in from the professors data object is between 1 and 3 or returns error message*/
   try {
-    if (professorData.profHotness >= 1 && professorData.profHotness <= 3) {
-      hotnessElement = HOTNESS_IMAGES[professorData.profHotness - 1];
+    if (prfData.profHotness >= 1 && prfData.profHotness <= 3) {
+      hotnessElement = HOTNESS_IMAGES[prfData.profHotness - 1];
     }else {
       throw{
       name: 'ProfessorHotnessOutOfRange',
@@ -65,18 +58,13 @@ var rmptip = function(professorElement, professorData) {
   }
 
   /* Contains the professor attributes in html elements  */
-  var profQuality = profDescriptionHeading('profcolumns',professorData.profQuality, 'Quality','');
-  var profAvgGrade = profDescriptionHeading('profcolumns',professorData.profAvgGrade, 'Avg Grade','');
+  var profQuality = profDescriptionHeading('profcolumns',prfData.profQuality, 'Quality','');
+  var profAvgGrade = profDescriptionHeading('profcolumns',prfData.profAvgGrade, 'Avg Grade','');
   var profHotness = profDescriptionHeading('profcolumns', hotnessElement, 'Hotness','');
-  var profHelpfulness = profDescriptionHeading('profLine', professorData.profHelpfulness, 'Helpfulness',progressBar);
-  var profClarity = profDescriptionHeading('profLine',professorData.profClarity, 'Clarity',progressBar);
-  var profEasiness = profDescriptionHeading('profLine',professorData.profEasiness, 'Easiness',progressBar);
+  var profHelpfulness = profDescriptionHeading('profLine', prfData.profHelpfulness, 'Helpfulness',progressBar);
+  var profClarity = profDescriptionHeading('profLine',prfData.profClarity, 'Clarity',progressBar);
+  var profEasiness = profDescriptionHeading('profLine',prfData.profEasiness, 'Easiness',progressBar);
   professorTipPopUp.setContent(profQuality + profAvgGrade + profHotness + profHelpfulness + profClarity + profEasiness); // Updates Opentips content
-  var canvas = document.querySelector('canvas'), ctx = canvas.getContext('2d');
-  //console.log(canvas);
-  // canvas.width = 700;
-  // canvas.height = 50;
-  // var progressBars = document.querySelector("canvas").nextElementSibling.childNodes[1];
   var progressBars = document.querySelector('#opentip-3');
   console.log(progressBars);
 };

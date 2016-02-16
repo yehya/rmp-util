a9ea4c4fde1aeaab74f6db7bd85cdba2d519c398
Yehya Awad <yehyaawad@hotmail.com>
2016-02-16 14:20:37 -0500

    Add docs for constant vals like HOTNESS_IMAGES, HOTNESS.COLD etc. Fixed styling using JSCS

--- rmp-tip.js
+++ rmp-tip.js
@@ -9,13 +9,27 @@
  */
 var rmptip = function(professorElement, prfData) {
 
-  // contains the hotness images
+  /**
+   * Image location sources
+   * @type {string[]}
+   */
   var HOTNESS_IMAGES = [
-      '<img src=\'assets/cold-chili.png\' height=\'25\' width=\'25\'>',
-      '<img src=\'assets/warm-chili.png\' height=\'25\' width=\'25\'>',
-      '<img src=\'assets/steamy-chili.png\' height=\'25\' width=\'25\'>',
-      '<img src=\'assets/scorching-chili.png\' height=\'25\' width=\'25\'>'    
-  ];
+     '<img src=\'assets/cold-chili.png\' height=\'25\' width=\'25\'>',
+     '<img src=\'assets/warm-chili.png\' height=\'25\' width=\'25\'>',
+     '<img src=\'assets/steamy-chili.png\' height=\'25\' width=\'25\'>',
+     '<img src=\'assets/scorching-chili.png\' height=\'25\' width=\'25\'>'
+ ];
+
+  /**
+   * Constants for chili pepper image to use
+   * @type {{COLD: number, WARM: number, STEAMY: number, SCORCHING: number}}
+   */
+  this.HOTNESS = {
+    COLD: 0,
+    WARM: 1,
+    STEAMY: 2,
+    SCORCHING: 3
+  };
 
   /*  creates a new instance of open tip which contains the pop up*/
 
@@ -51,18 +65,18 @@ var rmptip = function(professorElement, prfData) {
     var width = widthPX + 'px';
     var color = ['linear-gradient(to right, #b51b58 0%, #ef2e72 100%)', 'linear-gradient(to right, #ff9c00 0%, #ffd42b 100%)', 'linear-gradient(to right, #849c1b 0%, #c8e744 100%)'];
     var colorChosen;
-    
+
     /* Determines what color each progress bar is */
-    switch(true){
-        case (rating >=0 && rating <=1):
-            colorChosen = color[0];
-            break;
-        case (rating >1 && rating <=3):
-            colorChosen = color[1];
-            break;
-        case (rating > 3 && rating <=5):
-            colorChosen = color[2];
-            break;
+    switch (true){
+      case (rating >= 0 && rating <= 1):
+        colorChosen = color[0];
+        break;
+      case (rating > 1 && rating <= 3):
+        colorChosen = color[1];
+        break;
+      case (rating > 3 && rating <= 5):
+        colorChosen = color[2];
+        break;
     }
     console.log(colorChosen);
     /*progress bar for the professor attributes*/
@@ -87,74 +101,74 @@ var rmptip = function(professorElement, prfData) {
 
   var hotnessElement, profQuality, profAvgGrade, profHotness, profHelpfulness, profClarity ,profEasiness;
   hotnessElement = HOTNESS_IMAGES[prfData.profHotness - 1];
-  
+
   /* Validates that the professors data that is passed in and returns a professorDataError message*/
-  var validateProfessorData = function(){
+  var validateProfessorData = function() {
     try {
-        if (prfData.profHotness < 0 || prfData.profHotness > 4) {
-            throw{
-                name: 'ProfessorHotnessOutOfRange',
-                message: 'Professor Hotness Range must be between 1 and 4'
-            };   
-        }
-        if(prfData.profQuality <= 0 || prfData.profQuality > 5){
-            throw{
-                name: 'ProfessorQualityOutOfRange',
-                message: 'Professor Quality Range must be between 1 and 5'
-            };
-        }
-        if(prfData.profAvgGrade <= 0 || prfData.profAvgGrade > 5){
-            throw{
-                name: 'ProfessorAvgGradeOutOfRange',
-                message: 'Professor Average Grade Range must be between 1 and 5'
-            };
-        }
-        if(prfData.profHelpfulness <= 0 || prfData.profHelpfulness > 5){
-            throw{
-                name: 'ProfessorHelpfulnessOutOfRange',
-                message: 'Professor Helpfulness Range must be between 1 and 5'
-            };
-        }
-        if(prfData.profClarity <= 0 || prfData.profClarity > 5){
-            throw{
-                name: 'ProfessorClarityOutOfRange',
-                message: 'Professor Clarity Range must be between 1 and 5'
-            };
-        }
-        if(prfData.profEasiness <= 0 || prfData.profEasiness > 5){
-            throw{
-                name: 'ProfessorEasinessOutOfRange',
-                message: 'Professor Easiness Range must be between 1 and 5'
-            };
-        }
+      if (prfData.profHotness < 0 || prfData.profHotness > 4) {
+        throw{
+          name: 'ProfessorHotnessOutOfRange',
+          message: 'Professor Hotness Range must be between 1 and 4'
+        };
+      }
+      if (prfData.profQuality <= 0 || prfData.profQuality > 5) {
+        throw{
+          name: 'ProfessorQualityOutOfRange',
+          message: 'Professor Quality Range must be between 1 and 5'
+        };
+      }
+      if (prfData.profAvgGrade <= 0 || prfData.profAvgGrade > 5) {
+        throw{
+          name: 'ProfessorAvgGradeOutOfRange',
+          message: 'Professor Average Grade Range must be between 1 and 5'
+        };
+      }
+      if (prfData.profHelpfulness <= 0 || prfData.profHelpfulness > 5) {
+        throw{
+          name: 'ProfessorHelpfulnessOutOfRange',
+          message: 'Professor Helpfulness Range must be between 1 and 5'
+        };
+      }
+      if (prfData.profClarity <= 0 || prfData.profClarity > 5) {
+        throw{
+          name: 'ProfessorClarityOutOfRange',
+          message: 'Professor Clarity Range must be between 1 and 5'
+        };
+      }
+      if (prfData.profEasiness <= 0 || prfData.profEasiness > 5) {
+        throw{
+          name: 'ProfessorEasinessOutOfRange',
+          message: 'Professor Easiness Range must be between 1 and 5'
+        };
+      }
     }catch (error) {
-        console.error(error.name + ' ' + error.message);
-        switch(error.name){
-            case "ProfessorHotnessOutOfRange":
-                 hotnessElement = "Invalid Data";
-                 break;
-            case "ProfessorQualityOutOfRange":
-                 prfData.profQuality = "Invalid Data";
-                 break;
-            case "ProfessorAvgGradeOutOfRange":
-                 prfData.profAvgGrade = "Invalid Data";
-                 break;
-            case "ProfessorHelpfulnessOutOfRange":
-                 prfData.profHelpfulness = "Invalid Data";
-                 break;
-            case "ProfessorClarityOutOfRange":
-                 prfData.profClarity = "Invalid Data";
-                 break;
-            case "ProfessorEasinessOutOfRange":
-                 prfData.profEasiness = "Invalid Data";
-                 break;
-        }   
+      console.error(error.name + ' ' + error.message);
+      switch (error.name){
+        case 'ProfessorHotnessOutOfRange':
+          hotnessElement = 'Invalid Data';
+          break;
+        case 'ProfessorQualityOutOfRange':
+          prfData.profQuality = 'Invalid Data';
+          break;
+        case 'ProfessorAvgGradeOutOfRange':
+          prfData.profAvgGrade = 'Invalid Data';
+          break;
+        case 'ProfessorHelpfulnessOutOfRange':
+          prfData.profHelpfulness = 'Invalid Data';
+          break;
+        case 'ProfessorClarityOutOfRange':
+          prfData.profClarity = 'Invalid Data';
+          break;
+        case 'ProfessorEasinessOutOfRange':
+          prfData.profEasiness = 'Invalid Data';
+          break;
+      }
     }
-  }
-  
-  validateProfessorData(); 
+  };
+
+  validateProfessorData();
   /* Contains the professor attributes in html elements  */
-  
+
   profQuality = profDescriptionHeading('profcolumns',prfData.profQuality, 'Quality','');
   profAvgGrade = profDescriptionHeading('profcolumns',prfData.profAvgGrade, 'Avg Grade','');
   profHotness = profDescriptionHeading('profcolumns', hotnessElement, 'Hotness','');
